7c76cbbd581ad6bc8cb8267014068f6c87ed24f1
karen banci <karenbanci1@gmail.com>
2022-05-19 14:21:32 -0700

    add files components and pages

---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,15 @@
+import "./navbar.css";
+
+const NavBar = () => {
+
+
+  return (
+    <nav className="nav">
+      <div className="nav-logo">
+        <img src={"../../images/reddit-logo.png"} alt="Reddit-logo" />
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
