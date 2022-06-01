d93c56115b729a0738501c96b5a27df46a54cd78
karen banci <karenbanci1@gmail.com>
2022-05-31 22:45:10 -0700

    fix app-context name

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import "./search.css";
 import "../buttons.css";
-import AppContext from "../../appcontext"
+import AppContext from "../../app-context"
 
 export function Search() {
   const [searchTerm, setSearchTerm] = useState('');
